3593ece7c8111225fc68781c01285d59df6056f7
Riley <rileyfox134@gmail.com>
2023-01-27 21:48:45 -0600

    CSS Take 2

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,32 +1,42 @@
 import React from "react";
 import "./Widgets.css";
 import {
-    TwitterTimelineEmbed,
-    TwitterShareButton,
-    TwitterTweetEmbed,
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
 } from "react-twitter-embed";
 import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
-    return (
-        <div className= "widgets" >
-            <div className = "widgets__input">
-                <SearchIcon className = "widgets__searchIcon" />
-                <input placeholder= "Search Twitter" type= "text" />
-            </div>
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
 
-            <div className="widgets__widgetContainer">
-            <h2>Proficient with: </h2>
+      <div className="widgets__widgetContainer">
+        <h2>Proficient with: </h2>
 
-            
+        <div class="row">
+          <div class="column"></div>
+          <div class="column"></div>
+          <div class="column"></div>
+        </div>
 
-            <TwitterTweetEmbed tweetId={"1337777796446380033"} />
+        <div class="row">
+          <div class="column"></div>
+          <div class="column"></div>
+          <div class="column"></div>
+        </div>
 
-            <TwitterTimelineEmbed
-            sourceType="profile"
-            screenName="rileyfox134"
-            options={{ height: 400 }}
-            />
+        <TwitterTweetEmbed tweetId={"1337777796446380033"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="rileyfox134"
+          options={{ height: 400 }}
+        />
       </div>
     </div>
   );
